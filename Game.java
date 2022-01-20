69eebc1a7a5084335093b54aeed1ce576d8587c6
Jason Lin <jlin31@stuy.edu>
2022-01-19 20:34:37 -0500

    Added down and up

--- Game.java
+++ Game.java
@@ -5,20 +5,34 @@ public class Game {
   public static int[][] board = new int[4][4];
 
   public static void main(String[] args) {
-    for (int i = 0; i < 6; i++) {
-      fill();
-      printBoardArray();
+    // Text.clear();
+    // Text.hideCursor();
+    // drawBorder();
+    // Text.go(4, 6);
+    // System.out.print(Text.colorize("4", Text.WHITE));
+    // Text.go(4, 15);
+    // System.out.print(Text.colorize("512", Text.WHITE));
+    // Text.go(27, 1);
+    // Text.showCursor();
+    // printBoardArray();
+    for (int i = 0; i < 12; i++) {
+      addToBoard();
     }
+    printBoardArray();
+    down();
+    printBoardArray();
+    up();
+    printBoardArray();
   }
 
   public static void drawBorder() {
     Text.clear();
     for (int i = 0; i < 5; i++) {
       int x = 1 + (i*6);
-      DrawScreen.drawHorizontal(x ,1, 45);
+      DrawScreen.drawHorizontal(x ,1, 41);
     }
     for (int i = 0; i < 5; i++) {
-      int x = 1 + (i*11);
+      int x = 1 + (i*10);
       DrawScreen.drawVertical(1, x, 25);
     }
   }
@@ -37,23 +51,71 @@ public class Game {
     return false;
   }
 
-  public static void fill() {
+  public static void addToBoard() {
     boolean filled = false;
     while (!filled) {
       int x = (int)(Math.random()*4);
       int y = (int)(Math.random()*4);
+      int z = (int)(Math.random()*8);
       if (board[x][y] == 0) {
         board[x][y] = 2;
+        if (z == 0) board[x][y] = 4;
         filled = true;
       }
     }
   }
 
-  public static void printBoardArray() {
-    for (int i = 0; i < 4; i++) {
-      System.out.print(Arrays.toString(board[i]) + " ");
+  public static void down() {
+    for (int col = 0; col <= 3; col++) {
+      for (int row = 3; row > 0; row--) {
+        if (board[row][col] == board[row-1][col]) {
+          board[row][col] = 2*(board[row][col]);
+          board[row-1][col] = 0;
+        }
+        shiftDown(col);
+      }
     }
-    System.out.println();
+  }
+
+  public static void shiftDown(int col) {
+    for (int i = 3; i > 0; i--) {
+      if (board[i][col] == 0) {
+        board[i][col] = board[i-1][col];
+        board[i-1][col] = 0;
+      }
+    }
+  }
+
+  public static void up() {
+    for (int col = 0; col <= 3; col++) {
+      for (int row = 0; row < 3; row++) {
+        if (board[row][col] == board[row+1][col]) {
+          board[row][col] = 2*(board[row][col]);
+          board[row+1][col] = 0;
+        }
+        shiftUp(col);
+      }
+    }
+  }
+
+  public static void shiftUp(int col) {
+    for (int i = 0; i < 3; i++) {
+      if (board[i][col] == 0) {
+        board[i][col] = board[i+1][col];
+        board[i+1][col] = 0;
+      }
+    }
+  }
+
+  public static void printBoardArray() {
+    String s = "";
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        s += board[i][j] + " ";
+      }
+      s += "\n";
+    }
+    System.out.println(s);
   }
 
 
