ca41bdc437a833b1288d16d47861cf1760070e54
Jason Lin <jlin31@stuy.edu>
2022-01-20 16:05:47 -0500

    Added list of to do methods

--- DrawScreen.java
+++ DrawScreen.java
@@ -7,7 +7,7 @@ public class DrawScreen {
     Text.hideCursor();
     Text.go(xcor, ycor);
     for (int i = 0; i < length; i++) {
-      System.out.print(Text.colorize(" ", Text.WHITE+Text.BACKGROUND));
+      System.out.print(Text.colorizeBackground(" ", 185, 171, 158));
     }
   }
 
@@ -16,7 +16,82 @@ public class DrawScreen {
     Text.go(xcor, ycor);
     for (int i = 0; i < length; i++) {
       Text.go(xcor+i, ycor);
-      System.out.print(Text.colorize(" ", Text.WHITE+Text.BACKGROUND));
+      System.out.print(Text.colorizeBackground(" ", 185, 171, 158));
     }
   }
+
+  public static void drawBlank(int xcor, int ycor) {
+    Text.go(xcor, ycor);
+    for (int i = 0; i < 5; i++) {
+      Text.go(xcor+i, ycor);
+      System.out.print(Text.colorizeBackground("         ", 245, 245, 220 ));
+    }
+  }
+
+  public static void draw2(int xcor, int ycor) {
+    Text.go(xcor, ycor);
+    for (int i = 0; i < 5; i++) {
+      Text.go(xcor+i, ycor);
+      System.out.print(Text.colorizeBackground("         ", 203, 191, 179));
+    }
+    Text.go(xcor+2, ycor + 4);
+    System.out.print(Text.colorizeBackground("2", 203, 191, 179, Text.BLACK, Text.BOLD));
+  }
+
+  public static void draw4(int xcor, int ycor) {
+    Text.go(xcor, ycor);
+    for (int i = 0; i < 5; i++) {
+      Text.go(xcor+i, ycor);
+      System.out.print(Text.colorizeBackground("         ", 237, 224, 201));
+    }
+    Text.go(xcor+2, ycor + 4);
+    System.out.print(Text.colorizeBackground("4", 237, 224, 201, Text.BLACK, Text.BOLD));
+  }
+
+  public static void draw8(int xcor, int ycor) {
+    Text.go(xcor, ycor);
+    for (int i = 0; i < 5; i++) {
+      Text.go(xcor+i, ycor);
+      System.out.print(Text.colorizeBackground("         ", 244, 175, 120));
+    }
+    Text.go(xcor+2, ycor + 4);
+    System.out.print(Text.colorizeBackground("8", 244, 175, 120, Text.BLACK, Text.BOLD));
+  }
+
+  public static void draw16(int xcor, int ycor) {
+
+  }
+
+  public static void draw32(int xcor, int ycor) {
+
+  }
+
+  public static void draw64(int xcor, int ycor) {
+
+  }
+
+  public static void draw128(int xcor, int ycor) {
+
+  }
+
+  public static void draw256(int xcor, int ycor) {
+
+  }
+
+  public static void draw512(int xcor, int ycor) {
+
+  }
+
+  public static void draw1024(int xcor, int ycor) {
+
+  }
+
+  public static void draw2048(int xcor, int ycor) {
+
+  }
+
+  public static void draw4096(int xcor, int ycor) {
+
+  }
+
 }
