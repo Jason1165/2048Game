e7791133a3fe45544a51fd098b25d4a1e0a47354
Jason Lin <jlin31@stuy.edu>
2022-01-21 17:18:01 -0500

    Scoreboard, title, and moving position of board

--- Game.java
+++ Game.java
@@ -4,6 +4,8 @@ import java.util.*;
 public class Game {
   public static int[][] board = new int[4][4];
   public static int score = 0;
+  public static final int STARTX = 3;
+  public static final int STARTY = 3;
 
   public static void main(String[] args) {
     run();
@@ -12,7 +14,7 @@ public class Game {
   public static void run() {
     Text.hideCursor();
     Text.clear();
-    Text.go(1,1);
+    Text.go(1 + STARTX, 1 + STARTY);
 
     Scanner in = new Scanner(System.in);
     String input = "";
@@ -26,11 +28,13 @@ public class Game {
       if (input.equalsIgnoreCase("d")) moved = right();
       if (moved) addToBoard();
       drawBoard();
-      Text.go(26, 1);
+      Text.go(26 + STARTX, 1 + STARTY);
+      System.out.println(Text.colorize("Current Score: " + score + "\n", Text.WHITE, Text.BOLD));
+      Text.go(28 + STARTX, 1 + STARTY);
       System.out.print(Text.colorize("Enter q or quit to end the game.", Text.WHITE));
-      Text.go(27, 1);
+      Text.go(29 + STARTX, 1 + STARTY);
       System.out.print(Text.colorize("Enter W, A, S, and D to moved up, left, down and right.", Text.WHITE));
-      Text.go(28, 1);
+      Text.go(30 + STARTX, 1 + STARTY);
       Text.showCursor();
       input = in.nextLine();
     }
@@ -40,33 +44,39 @@ public class Game {
     Text.clear();
     for (int i = 0; i < 5; i++) {
       int x = 1 + (i*6);
-      DrawScreen.drawHorizontal(x ,1, 41);
+      DrawScreen.drawHorizontal(x + STARTX ,1 + STARTY , 41);
     }
     for (int i = 0; i < 5; i++) {
       int x = 1 + (i*10);
-      DrawScreen.drawVertical(1, x, 25);
+      DrawScreen.drawVertical(1 + STARTX, x + STARTY, 25);
     }
+  }
+
+  public static void drawTitle() {
+    Text.go(2, 8);
+    System.out.print(Text.colorize("The Terminal Version of 2048 Game", Text.WHITE, Text.BOLD));
   }
 
   public static void drawBoard() {
     Text.clear();
     drawBorder();
+    drawTitle();
     Text.hideCursor();
     for (int row = 0; row < 4; row++) {
       for (int col = 0; col < 4; col++) {
-        if (board[row][col] == 0) DrawScreen.drawBlank(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 2) DrawScreen.draw2(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 4) DrawScreen.draw4(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 8) DrawScreen.draw8(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 16) DrawScreen.draw16(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 32) DrawScreen.draw32(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 64) DrawScreen.draw64(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 128) DrawScreen.draw128(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 256) DrawScreen.draw256(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 512) DrawScreen.draw512(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 1024) DrawScreen.draw1024(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 2048) DrawScreen.draw2048(row*6 + 2, col*10 + 2);
-        else if (board[row][col] == 4096) DrawScreen.draw4096(row*6 + 2, col*10 + 2);
+        if (board[row][col] == 0) DrawScreen.drawBlank(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 2) DrawScreen.draw2(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 4) DrawScreen.draw4(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 8) DrawScreen.draw8(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 16) DrawScreen.draw16(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 32) DrawScreen.draw32(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 64) DrawScreen.draw64(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 128) DrawScreen.draw128(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 256) DrawScreen.draw256(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 512) DrawScreen.draw512(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 1024) DrawScreen.draw1024(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 2048) DrawScreen.draw2048(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
+        else if (board[row][col] == 4096) DrawScreen.draw4096(row*6 + 2 + STARTX, col*10 + 2 + STARTY);
       }
     }
   }
